266ed49a547ed80549df573aec8b6ac5035f4a02
isaac <isaaczeledon01@outlook.com>
2025-11-03 20:48:07 -0600

    1
    error git

---
+++ Sistema-de-agricultura-naciona/Sistema de Venta agricultura/Registro.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Sistema_de_Venta_agricultura
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtNombre = new TextBox();
+            txtCedula = new TextBox();
+            txtCorreo = new TextBox();
+            txtContra = new TextBox();
+            btnRegistrar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(321, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(362, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Pag De Registro del Agricultor";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(226, 185);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(233, 238);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Cedula";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(234, 295);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Correo";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(226, 353);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Contraseña";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(321, 177);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(117, 23);
+            txtNombre.TabIndex = 6;
+            // 
+            // txtCedula
+            // 
+            txtCedula.Location = new Point(321, 230);
+            txtCedula.Name = "txtCedula";
+            txtCedula.Size = new Size(100, 23);
+            txtCedula.TabIndex = 8;
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(321, 292);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(100, 23);
+            txtCorreo.TabIndex = 9;
+            // 
+            // txtContra
+            // 
+            txtContra.Location = new Point(312, 350);
+            txtContra.Name = "txtContra";
+            txtContra.Size = new Size(100, 23);
+            txtContra.TabIndex = 10;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(233, 421);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(75, 23);
+            btnRegistrar.TabIndex = 11;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // Registro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1039, 631);
+            Controls.Add(btnRegistrar);
+            Controls.Add(txtContra);
+            Controls.Add(txtCorreo);
+            Controls.Add(txtCedula);
+            Controls.Add(txtNombre);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Registro";
+            Text = "Registro";
+            Load += Registro_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtNombre;
+        private TextBox txtCedula;
+        private TextBox txtCorreo;
+        private TextBox txtContra;
+        private Button btnRegistrar;
+    }
+}
